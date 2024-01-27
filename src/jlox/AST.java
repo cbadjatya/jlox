3fb72aa2feaf5aa66fd6d8e843264f3e372eebfd
cs22m036 <cs22m036@smail.iitm.ac.in>
2024-01-27 15:09:09 +0530

    finished implementing Visitor pattern partially

---
+++ src/jlox/AST.java
@@ -0,0 +1,85 @@
+package jlox;
+
+abstract class Expr{
+
+	interface Visitor<R> {
+		R visitBinaryExpr(Binary expr);
+		R visitUnaryExpr(Unary expr);
+		R visitGroupingExpr(Grouping expr);
+		R visitLiteralExpr(Literal expr);
+	}
+
+
+	abstract <R> R accept(Visitor<R> visitor);
+
+	static class Binary extends Expr{
+
+		final Expr left;
+		final Expr right;
+		final Token op;
+
+		Binary( Expr left, Expr right, Token op ){
+
+			this.left = left;
+			this.right = right;
+			this.op = op;
+
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitBinaryExpr(this);
+		}
+	}
+
+	static class Unary extends Expr{
+
+		final Expr right;
+		final Token op;
+
+		Unary( Expr right, Token op ){
+
+			this.right = right;
+			this.op = op;
+
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitUnaryExpr(this);
+		}
+	}
+
+	static class Grouping extends Expr{
+
+		final Expr expr;
+
+		Grouping( Expr expr ){
+
+			this.expr = expr;
+
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitGroupingExpr(this);
+		}
+	}
+
+	static class Literal extends Expr{
+
+		final Object value;
+
+		Literal( Object value ){
+
+			this.value = value;
+
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitLiteralExpr(this);
+		}
+	}
+
+}
---
+++ src/jlox/ASTPrinter.java
@@ -0,0 +1,62 @@
+package jlox;
+
+import jlox.Expr.Binary;
+import jlox.Expr.Grouping;
+import jlox.Expr.Literal;
+import jlox.Expr.Unary;
+
+class ASTPrinter implements Expr.Visitor<String>{
+	
+	String print(Expr expr) {
+		return expr.accept(this);
+	}
+	
+	@Override
+	public String visitBinaryExpr(Binary expr) {
+		return parenthesize(expr.op.lexeme, expr.left, expr.right);
+	}
+
+	@Override
+	public String visitUnaryExpr(Unary expr) {
+		// TODO Auto-generated method stub
+		return parenthesize(expr.op.lexeme, expr.right);
+	}
+
+	@Override
+	public String visitGroupingExpr(Grouping expr) {
+		// TODO Auto-generated method stub
+		return parenthesize("group",expr.expr);
+	}
+
+	@Override
+	public String visitLiteralExpr(Literal expr) {
+		if(expr.value == null) return "nil";
+		return expr.value.toString();
+	}
+	
+	public String parenthesize(String name, Expr...exprs) {
+		String s = "";
+		s += " ( ";
+		s += name + " ";
+		for(Expr ex : exprs) {
+			s += ex.accept(this);
+		}
+		s += " ) ";
+		return s;
+	}
+	
+	public static void main(String args[]) {
+		Expr expression = new Expr.Binary(
+		new Expr.Unary(
+		new Expr.Literal(123),
+		new Token(TokenType.MINUS, "-", null, 1)),
+		new Expr.Grouping(
+		new Expr.Literal(45.67)),
+		new Token(TokenType.STAR, "*", null, 1));
+		
+		System.out.println(new ASTPrinter().print(expression));
+	}
+}
+
+
+
